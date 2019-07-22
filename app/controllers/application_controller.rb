63d10ccb6f037d60b3d11c69b2bca7486b85e048
Matteo Muscas <thatman.j@gmail.com>
2019-07-22 10:36:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    flash[:error] = "Access denied."
+    redirect_to(request.referrer || root_path)
+  end
 
 
 end
